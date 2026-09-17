736834122b0e97c3a93d957b95878a24be6cc0e8
louaiTiaraGroup <147396267+louaiTiaraGroup@users.noreply.github.com>
2026-09-17 11:57:33 +0000

    chore: applied pint changes

--- packages/Webkul/Admin/src/DataGrids/Quote/QuoteDataGrid.php
+++ packages/Webkul/Admin/src/DataGrids/Quote/QuoteDataGrid.php
@@ -2,6 +2,7 @@
 
 namespace Webkul\Admin\DataGrids\Quote;
 
+use Carbon\Carbon;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\DB;
 use Webkul\Contact\Repositories\PersonRepository;
@@ -92,11 +93,11 @@ class QuoteDataGrid extends DataGrid
             'type'       => 'date',
             'sortable'   => true,
             'filterable' => true,
-           'closure' => fn ($row) => $row->event_at
-            ? \Carbon\Carbon::parse($row->event_at)
-                ->locale(app()->getLocale())
-                ->translatedFormat(app()->getLocale() === 'ar' ? 'd F Y' : 'd M Y')
-            : '—',
+            'closure'    => fn ($row) => $row->event_at
+             ? Carbon::parse($row->event_at)
+                 ->locale(app()->getLocale())
+                 ->translatedFormat(app()->getLocale() === 'ar' ? 'd F Y' : 'd M Y')
+             : '—',
         ]);
 
         $this->addColumn([
@@ -205,8 +206,8 @@ class QuoteDataGrid extends DataGrid
             'searchable' => false,
             'sortable'   => true,
             'filterable' => true,
-            'closure' => fn ($row) => $row->expired_at
-            ? \Carbon\Carbon::parse($row->expired_at)
+            'closure'    => fn ($row) => $row->expired_at
+            ? Carbon::parse($row->expired_at)
                 ->locale(app()->getLocale())
                 ->translatedFormat(app()->getLocale() === 'ar' ? 'd F Y' : 'd M Y')
             : '—',
@@ -220,8 +221,8 @@ class QuoteDataGrid extends DataGrid
             'searchable' => false,
             'sortable'   => true,
             'filterable' => true,
-       'closure' => fn ($row) => $row->created_at
-    ? \Carbon\Carbon::parse($row->created_at)
+            'closure'    => fn ($row) => $row->created_at
+    ? Carbon::parse($row->created_at)
         ->locale(app()->getLocale())
         ->translatedFormat(app()->getLocale() === 'ar' ? 'd F Y - h:i A' : 'd M Y - h:i A')
     : '—',
